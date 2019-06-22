416b973d9b73340ff62ad1466a2ae4849af27105
Apollinaire <no-reply@apollinai.re>
2019-06-22 12:17:58 +0200

    wip bis

---
+++ packages/graphql-schemas/lib/components/Dagre.jsx
@@ -0,0 +1,104 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import * as dagreD3 from 'dagre-d3'
+import * as d3 from 'd3'
+
+import isEqual from 'react-fast-compare'
+
+class DagreD3 extends React.Component {
+
+    static defaultProps = {
+        height: "1",
+        width: "1",
+        // width and height are defaulted to 1 due to a FireFox bug(?) If set to 0, it complains.
+        fit: true,
+        interactive: false
+    };
+
+    static propTypes = {
+        nodes: PropTypes.object.isRequired,
+        edges: PropTypes.array.isRequired,
+        interactive: PropTypes.bool,
+        fit: PropTypes.bool,
+        height: PropTypes.string,
+        width: PropTypes.string,
+        shapeRenderers: PropTypes.objectOf(PropTypes.func),
+        onNodeClick: PropTypes.func,
+    };
+
+    shouldComponentUpdate(nextProps, nextState) {
+        return !isEqual(this.props.nodes, nextProps.nodes) ||
+            !isEqual(this.props.edges, nextProps.edges) ||
+            !isEqual(this.props.zoom, nextProps.zoom)
+    }
+
+    componentDidMount() {
+        this.renderDag();
+    }
+
+    componentDidUpdate() {
+        this.renderDag();
+    }
+
+    renderDag() {
+        let g = new dagreD3.graphlib.Graph().setGraph({});
+
+
+        for (let [id, node] of Object.entries(this.props.nodes))
+            g.setNode(id, node);
+
+        for (let edge of this.props.edges)
+            g.setEdge(edge[0], edge[1], edge[2]); // from, to, props
+
+        // Set up an SVG group so that we can translate the final graph.
+        let svg = d3.select(this.nodeTree);
+        let inner = d3.select(this.nodeTreeGroup);
+
+        // set up zoom support
+        if (this.props.interactive) {
+            let zoom = d3.zoom().on("zoom",
+                () => inner.attr("transform", d3.event.transform));
+            svg.call(zoom);
+        }
+
+        // Create the renderer
+        let render = new dagreD3.render();
+
+        // set up custom shape renderers
+        if (this.props.shapeRenderers)
+            for (let [shape, renderer] of Object.entries(this.props.shapeRenderers))
+                render.shapes()[shape] = renderer;
+
+        // Run the renderer. This is what draws the final graph.
+        render(inner, g);
+
+
+        // TODO add padding?
+        if (this.props.fit) {
+            let {height: gHeight, width: gWidth} = g.graph();
+            let {height, width} = this.nodeTree.getBBox();
+            let transX = width - gWidth;
+            let transY = height - gHeight;
+            svg.attr("height", height);
+            svg.attr("width", width);
+            inner.attr("transform", d3.zoomIdentity.translate(transX, transY))
+        }
+
+        if (this.props.onNodeClick)
+            svg.selectAll('.dagre-d3 .node').on('click',
+                    id => this.props.onNodeClick(id));
+    }
+
+    render() {
+        return (
+            <svg className='dagre-d3' ref={(r) => {this.nodeTree = r}}
+                 width={this.props.height}
+                 height={this.props.width}>
+
+                <g ref={(r) => {this.nodeTreeGroup = r}}/>
+            </svg>
+        );
+    }
+}
+
+export default DagreD3;
---
+++ packages/graphql-schemas/lib/components/Voyager.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class Voyager extends React.Component {
+  render() {
+    return <p>plop</p>;
+  }
+}
+
+export default Voyager;
