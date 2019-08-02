4947784c04a88823b8e1c3f609744d91fd402b96
Apollinaire <no-reply@apollinai.re>
2019-08-02 13:09:35 +0200

    fix missing package versions

--- packages/graphql-schemas/package.js
+++ packages/graphql-schemas/package.js
@@ -9,12 +9,12 @@ Package.onUse(function (api) {
     'promise',
 
     // vulcan core
-    'vulcan:core',
+    'vulcan:core@1.13.1',
 
     // vulcan packages
-    'vulcan:forms',
-    'vulcan:accounts',
-    'vulcan:ui-bootstrap',
+    'vulcan:forms@1.13.1',
+    'vulcan:accounts@1.13.1',
+    'vulcan:ui-bootstrap@1.13.1',
     
   ]);
 
