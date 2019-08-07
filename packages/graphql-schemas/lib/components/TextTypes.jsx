5e064eeff435c89f9e461598b664b1c4c4a4db78
Apollinaire <no-reply@apollinai.re>
2019-08-07 17:19:26 +0200

    init sort types

--- packages/graphql-schemas/lib/components/TextTypes.jsx
+++ packages/graphql-schemas/lib/components/TextTypes.jsx
@@ -1,29 +1,47 @@
 import React from 'react';
 import _map from 'lodash/map';
+import _orderBy from 'lodash/orderBy';
 
-const TypeDefinition = ({name, fields}) => {
-
+const TypeDefinition = ({ name, fields }) => {
   return (
     <div className='col'>
       <h1>{name}</h1>
       <ul>
-        {_map(fields, (value, key)=> <li key={key}>{key}</li> )}
+        {_map(fields, (value, key) => (
+          <li key={key}>{key}</li>
+        ))}
       </ul>
     </div>
-    )
-}
+  );
+};
 
-class TextTypes extends React.Component {
+const iteratees = [
+  v => (v.name === 'Query' ? 1 : v.name === 'Mutation' ? 2 : 3),
+  v => v.name.charAt(0) === '_' ? 1 : 0,
+  'kind',
+  'name',
+];
+
+const orders = [
+  'asc', 'asc', 'desc', 'desc'
+]
+
+class TextTypes extends React.PureComponent {
   render() {
     const { simpleSchema } = this.props;
-    if(!simpleSchema) return 'nothing'
+    if (!simpleSchema) return 'nothing';
+    const types = _orderBy(simpleSchema.types, iteratees, orders);
     return (
-    <div className='container'>
-      <div className='row'>
-      <TypeDefinition {...simpleSchema.queryType}/>
-      <TypeDefinition {...simpleSchema.mutationType}/>
+      <div>
+        <div className='row'>
+          {/* <TypeDefinition {...simpleSchema.queryType} />
+          <TypeDefinition {...simpleSchema.mutationType} /> */}
+          {_.map(types, type => (
+            <TypeDefinition key={type.name} {...type} />
+          ))}
+        </div>
       </div>
-    </div>);
+    );
   }
 }
 
